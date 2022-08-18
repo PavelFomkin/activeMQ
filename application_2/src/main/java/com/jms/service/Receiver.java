8bddd0a4439d58f3ed7365662e5159f1591869aa
Pavel_Fomkin <pavel_fomkin@epam.com>
2022-08-18 10:43:02 +0300

    task1 changed logs from sout to log

--- application_2/src/main/java/com/jms/service/Receiver.java
+++ application_2/src/main/java/com/jms/service/Receiver.java
@@ -1,13 +1,16 @@
 package com.jms.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
     @JmsListener(destination = "${jms.topic-name}", containerFactory = "jmsContainerFactory")
     public void receiveMessage(String message) {
-        System.out.println("[1] Message received is: " + message);
+        LOGGER.info("[Receiver 1] Message received is: {}", message);
     }
 }

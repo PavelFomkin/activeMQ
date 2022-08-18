f10f0410d89f3fd5c054651a7dc4dc491bc0df71
Pavel_Fomkin <pavel_fomkin@epam.com>
2022-08-18 10:50:31 +0300

    task1 renamed Receivers to MessageReceivers

--- application_2/src/main/java/com/jms/service/MessageReceiver.java
+++ application_2/src/main/java/com/jms/service/MessageReceiver.java
@@ -6,11 +6,11 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Receiver2 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver2.class);
+public class MessageReceiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageReceiver.class);
 
     @JmsListener(destination = "${jms.topic-name}", containerFactory = "jmsContainerFactory")
     public void receiveMessage(String message) {
-        LOGGER.info("[Receiver 2] Message received is: {}", message);
+        LOGGER.info("[Receiver 1] Message received is: {}", message);
     }
 }
--- application_2/src/main/java/com/jms/service/MessageReceiver2.java
+++ application_2/src/main/java/com/jms/service/MessageReceiver2.java
@@ -6,11 +6,11 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Receiver {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+public class MessageReceiver2 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageReceiver2.class);
 
     @JmsListener(destination = "${jms.topic-name}", containerFactory = "jmsContainerFactory")
     public void receiveMessage(String message) {
-        LOGGER.info("[Receiver 1] Message received is: {}", message);
+        LOGGER.info("[Receiver 2] Message received is: {}", message);
     }
 }

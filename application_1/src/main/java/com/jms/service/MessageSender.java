41f8ef5b7e435961521be2785462e7bedbab1eea
Pavel_Fomkin <pavel_fomkin@epam.com>
2022-08-21 16:48:07 +0300

    task 2 implemented real temp queue

--- application_1/src/main/java/com/jms/service/MessageSender.java
+++ application_1/src/main/java/com/jms/service/MessageSender.java
@@ -8,20 +8,44 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.jms.*;
+
 @Service
 public class MessageSender {
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageSender.class);
 
-    @Value("${jms.requestChanel}")
+    @Value("${jms.destination}")
     private String destination;
+    private Session session;
 
     @Autowired
     private JmsTemplate jmsTemplate;
+    @Autowired
+    private ConnectionFactory connectionFactory;
 
     @SneakyThrows
     public void sendMessage(String message) {
-        jmsTemplate.convertAndSend(destination, message);
+        if (session == null) {
+            init();
+        }
 
+        TextMessage textMessage = session.createTextMessage();
+        textMessage.setText(message);
+        TemporaryQueue tempQueue = session.createTemporaryQueue();
+        textMessage.setJMSReplyTo(tempQueue);
+        jmsTemplate.convertAndSend(destination, textMessage);
         LOGGER.info("Message \"{}\" sent to: {}", message, destination);
+
+        Message receive = session.createConsumer(tempQueue).receive();
+        String text = ((TextMessage) receive).getText();
+        LOGGER.info("Reply message received is: {}", text);
+    }
+
+    @SneakyThrows
+    private void init() {
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
     }
 }

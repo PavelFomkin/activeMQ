24eb1cb2e78dff1df8d6f78a361ab8ddcd981dff
Pavel_Fomkin <pavel_fomkin@epam.com>
2022-08-26 18:30:50 +0300

    rabbitMQ task1: removed unnecessary @SneakyThrow annotations

--- src/main/java/com/jms/service/MessageReceiver.java
+++ src/main/java/com/jms/service/MessageReceiver.java
@@ -42,7 +42,6 @@ public class MessageReceiver {
         System.out.println("[2] listenerWithError: message processed: " + payload);
     }
 
-    @SneakyThrows
     @RabbitListener(
             bindings = @QueueBinding(
                     value = @Queue(name = "processedQueue", durable = "true"),
@@ -57,7 +56,6 @@ public class MessageReceiver {
         System.out.println("[1] listener: message processed: " + payload);
     }
 
-    @SneakyThrows
     @RabbitListener(
             bindings = @QueueBinding(
                     value = @Queue(name = "failureQueue", durable = "true"),
